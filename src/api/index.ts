053368c29c3fa1996c22d517db3f11e88656cc26
Kyle Reed <kylejreed2@gmail.com>
2022-05-29 20:25:06 -0700

    exporting auth router

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,9 @@
 import GroupsRouter from "./groups/routes"
 import UserRouter from "./users/routes"
+import AuthRouter from "./auth/routes"
 
 import { handleErrorsMiddleware } from "./middleware/error-handling"
 
 // Registered in order
 export const middlewares = [handleErrorsMiddleware]
-export const routes = [UserRouter, GroupsRouter]
+export const routes = [AuthRouter, UserRouter, GroupsRouter]
